643e8553d4d5ed2c4557f29d7bf96aacf43e7457
jsyeh <jsyeh@mail.mcu.edu.tw>
2022-04-08 12:46:03 +0800

    update week07

--- week07/week07-7.cpp
+++ week07/week07-7.cpp
@@ -1,8 +1,13 @@
-//Week07-7.cpp step03-3 完成數數
+///Week07-7.cpp step03-3 最後統計
+///最後我們統計出各個國家出現的次數,
+///利用迴圈,把上下2筆資料 strcmp(line[i],line[i+1])==0看是否相同
+///相同就combo++,不相同就可以印出 line[i] 那個國家對應的 combo數量。
+///裡面有2個地方比較特別,分別是迴圈跑到什麼時候結束,
+///及對應的 line[N]的「哨兵sentinal」在超過的地方標註成空字串
 #include <stdio.h>
 #include <stdlib.h> //qsort()
 #include <string.h> //strcmp()
-char line[2000][80];
+char line[2001][80];
 int compare( const void *p1, const void *p2 )
 {
 	return strcmp( (char*)p1, (char*)p2 );
